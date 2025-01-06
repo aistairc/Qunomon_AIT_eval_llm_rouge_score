1d8af0835956baadc44849361d3734c005dffc3f
Darren.s@gcsystem.co.jp <Xiao&2441>
2025-01-06 11:12:45 +0900

    update

--- deploy/container/repository/my_ait.py
+++ deploy/container/repository/my_ait.py
@@ -177,7 +177,7 @@ if not is_ait_launch:
     inventory_requirement_model = manifest_genenerator.format_ait_inventory_requirement(format_=['ALL'])
     manifest_genenerator.add_ait_inventories(name='llm_model_dir', 
                                               type_='model', 
-                                              description='事前にトレーニング済みの大規模言語モデルと、そのモデルの設定ファイルを保存したディレクトリ\n 例:T5, GPT-3\n モデルファイルは、config.json, model.safetensors, generation_config.json, special_tokens_map.json, tokenizer_config.json, tokenizer.jsonを含む', 
+                                              description='事前トレーニング済みの大規模言語モデルと設定ファイルのディレクトリ（例: HuggingfaceのT5）\n必要なファイルはconfig.json, model.safetensors, generation_config.json, special_tokens_map.json, tokenizer_config.json, tokenizer.jsonを含む', 
                                               requirement=inventory_requirement_model)
     manifest_genenerator.add_ait_measures(name='ROUGE1_Score', 
                                            type_='float', 
